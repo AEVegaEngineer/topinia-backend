cd6dfba9e9bc14f7e205e9d59f9f447fdccb0f83
Andres Vega <aevega1991@gmail.com>
2024-12-21 19:37:08 -0300

    feat: :sparkles: added logger on topics controller and service

--- src/opinion-management-service/src/topics/topics.controller.ts
+++ src/opinion-management-service/src/topics/topics.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post, Put } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Put } from '@nestjs/common';
 import { TopicsService } from './topics.service';
 import { CreateTopicDto, UpdateTopicDto } from './dto';
+
+const logger = new Logger('TopicsController');
 
 @Controller('topics')
 export class TopicsController {
@@ -8,11 +10,13 @@ export class TopicsController {
 
   @Post('create')
   createTopic(@Body() createTopicDto: CreateTopicDto) {
+    logger.log(`createTopic ${JSON.stringify(createTopicDto)}`);
     return this.topicsService.create(createTopicDto);
   }
 
   @Put('update')
   updateTopic(@Body() updateTopicDto: UpdateTopicDto) {
+    logger.log(`updateTopic ${JSON.stringify(updateTopicDto)}`);
     return this.topicsService.update(updateTopicDto);
   }
 }
--- src/opinion-management-service/src/topics/topics.service.ts
+++ src/opinion-management-service/src/topics/topics.service.ts
@@ -1,26 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateTopicDto } from './dto/create-topic.dto';
 import { UpdateTopicDto } from './dto/update-topic.dto';
 
+const logger = new Logger('TopicsService');
 @Injectable()
 export class TopicsService {
   create(createTopicDto: CreateTopicDto) {
+    logger.log(`create ${JSON.stringify(createTopicDto)}`);
     return 'This action adds a new topic';
   }
 
   findAll() {
+    logger.log('findAll');
     return `This action returns all topics`;
   }
 
   findOne(id: number) {
+    logger.log(`findOne ${id}`);
     return `This action returns a #${id} topic`;
   }
 
   update(updateTopicDto: UpdateTopicDto) {
+    logger.log(`update ${JSON.stringify(updateTopicDto)}`);
     return `This action updates a #${updateTopicDto.topic_id} topic`;
   }
 
   remove(id: number) {
+    logger.log(`remove ${id}`);
     return `This action removes a #${id} topic`;
   }
 }
