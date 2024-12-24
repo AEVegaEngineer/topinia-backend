5b8d210ef47c95d6f72fb7431860fbf8c960aaad
Andres Vega <aevega1991@gmail.com>
2024-12-24 18:34:53 -0300

    feat: :sparkles: setting up database access

--- src/opinion-management-service/app.module.ts
+++ src/opinion-management-service/app.module.ts
@@ -1,8 +1,37 @@
 import { Module } from '@nestjs/common';
 import { TopicsModule } from './src/topics/topics.module';
 import { UsersModule } from './src/auth/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TopicsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    /**
+     * Con este metodo tiene todas las configuraciones para nuestra conexión
+     * con la BD (puedes ver todas las opciones que nos ofrece typeOrm acá:
+     * https://typeorm.io/data-source-options)
+     */
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // al cambiar entidades en codigo se cambian en la DB
+    }),
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    TopicsModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
