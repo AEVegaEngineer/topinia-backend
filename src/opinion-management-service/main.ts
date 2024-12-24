d694bdca3414d5c291b146a56d4291e5a3ee5044
Andres Vega <aevega1991@gmail.com>
2024-12-23 23:17:03 -0300

    fix: :bug: fixed typo

--- src/opinion-management-service/main.ts
+++ src/opinion-management-service/main.ts
@@ -3,9 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { Context, Handler } from 'aws-lambda';
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
 
 const logger = new Logger('OpinionManagementService');
 const PORT = 4000;
+let cachedServer: Server;
 
 async function bootstrap() {
   const expressApp = express();
@@ -17,7 +21,15 @@ async function bootstrap() {
     logger.log(`Opinion Management Service is listening on  127.0.0.1:${PORT}`);
   } else {
     await app.init();
+    return createServer(expressApp);
   }
 }
+
+export const handler: Handler = async (event: any, context: Context) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
 
 bootstrap();
