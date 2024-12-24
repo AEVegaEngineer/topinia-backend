cc2cc1e85db491ffd588f013aaaa4eb03dc074be
Andres Vega <aevega1991@gmail.com>
2024-12-24 15:58:48 -0300

    feat: :sparkles: Added user-interest entity

---
+++ src/opinion-management-service/src/user_interest/entities/user-interest.entity.ts
@@ -0,0 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Interest } from '../../interest/entities/interest.entity';
+
+@Entity({ name: 'userInterest' })
+export class UserInterest {
+  @ApiProperty({
+    example: '1022c266-8803-4916-a20c-08bb2a6ebf17',
+    description: 'Social Media Registry ID',
+    uniqueItems: true,
+  })
+  @PrimaryGeneratedColumn('uuid')
+  userInterestId: string;
+
+  @ApiProperty({
+    example: '1022c266-8803-4916-a20c-08bb2a6ebf17',
+    description: 'Foreign Key: User reference ID, this user has this interest',
+  })
+  @Column('text')
+  userInterestUserId: string;
+
+  @ApiProperty({
+    example: '1022c266-8803-4916-a20c-08bb2a6ebf17',
+    description: 'Foreign Key: Interest reference ID',
+  })
+  @Column('text')
+  interestId: string;
+}
